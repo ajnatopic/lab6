9f00f00909e58a9d42afc85099d626285fc84032
ajnatopic <ajnatopic2@gmail.com>
2023-10-28 23:19:08 -0400

    This is my first addition to the repository!

--- ajna.py
+++ ajna.py
@@ -15,49 +15,49 @@ def encode(password):
 
 def decode():
     pass
+if __name__ == "__main__":
 
-
-print("Menu\n"
-"-------------\n"
-"1. Encode\n"
-"2. Decode\n"
-"3. Quit\n")
-
-menu_one=0
-o = int(input("Please enter an option: "))
-while o != 3:
-
-    if o == 1:
-        try:
-                menu_one=1
-                password = input("Please enter your password to encode: ")
-                encoded=encode(password)
-                print("Your password has been encoded and stored!")
-        except ValueError as excpt:
-                print(excpt)
-                print('Add a new password')
-    elif o == 2:
-        if menu_one == 1:
-            try:
-                print(f"The encoded password is {encoded}, and the original password is ORIGINALPASSWORD.")
-            except ValueError as excpt:
-                print(excpt)
-                print('Add a new password')
-
-        else:
-            print("Menu one must be called before menu two.")
-
-    elif o == 3:
-        break
-    else:
-        print("Invalid input.")
-
-    print("\nMenu\n"
+    print("Menu\n"
     "-------------\n"
     "1. Encode\n"
     "2. Decode\n"
-    "3. Quit")
-    o = int(input("\nPlease enter an option: "))
+    "3. Quit\n")
+
+    menu_one=0
+    o = int(input("Please enter an option: "))
+    while o != 3:
+
+        if o == 1:
+            try:
+                    menu_one=1
+                    password = input("Please enter your password to encode: ")
+                    encoded=encode(password)
+                    print("Your password has been encoded and stored!")
+            except ValueError as excpt:
+                    print(excpt)
+                    print('Add a new password')
+        elif o == 2:
+            if menu_one == 1:
+                try:
+                    print(f"The encoded password is {encoded}, and the original password is ORIGINALPASSWORD.")
+                except ValueError as excpt:
+                    print(excpt)
+                    print('Add a new password')
+
+            else:
+                print("Menu one must be called before menu two.")
+
+        elif o == 3:
+            break
+        else:
+            print("Invalid input.")
+
+        print("\nMenu\n"
+        "-------------\n"
+        "1. Encode\n"
+        "2. Decode\n"
+        "3. Quit")
+        o = int(input("\nPlease enter an option: "))
 
 
 
